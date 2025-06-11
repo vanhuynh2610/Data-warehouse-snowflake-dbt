9698fb5bd7e3293e14e92e5e85c6b8a5aa4a9ef4
vanhuynh2610 <huynhthanhvan2610@gmail.com>
2025-06-11 21:00:53 +0700

    Delete Airflow/airflow_v1/dags/postgres_test.py

--- Airflow/airflow_v1/dags/postgres_test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-
-from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-
-
